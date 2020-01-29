d7063452eb84bc771cef8ed30823c0b73193cea1
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2020-01-28 23:29:22 -0700

    This commit adds the functionality for passing variables between servers

---
+++ client/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+
+    render(){
+        return(
+        <div>Hello react</div>
+        )
+        }
+}
+     
+export default App
+ReactDOM.render(<App />, document.getElementById("app"));
--- server/server2.js
+++ server/server2.js
@@ -5,6 +5,13 @@ var app = express();
 app.listen(3200, function() {
     console.log('Listening on port 3200');
 }) 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 app.use(bodyParser.json());   
 app.use(bodyParser.urlencoded(extended = true));
 app.use(express.static(path.join(__dirname, 'client')));
