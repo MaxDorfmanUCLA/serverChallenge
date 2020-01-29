fa80435875d70891133c5637c4c1e7f70adba7a2
MaxDorfmanUCLA <max.dorfman1@gmail.com>
2020-01-29 02:41:57 -0700

    Almost finished implementing auto loading from the mongo db to show the feed from the other messages

--- server/server1.js
+++ server/server1.js
@@ -17,10 +17,55 @@ app.use(bodyParser.urlencoded(extended = true));
 app.use(express.static(path.join(__dirname, 'client')));
 
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/serverChallenge');
+var db = mongoose.connection;
+db.on('error', function (err) {
+    console.log('connection error', err);
+});
+db.once('open', function () {
+    console.log('connected.');
+});
+var Schema = mongoose.Schema;
+// create a schema
+var userSchema = new Schema({
+  sender: String,
+  value: String
+});
+var User = mongoose.model('User', userSchema);
+
+
 app.get('/', (req, res) => {
+    User.find({}, function(err, users) {
+        if (err) throw err;
+      
+        // object of all the users
+        console.log(users);
+      });
+    res.status(201).send('posts loaded from db');
+    // User.find({}, function(err, users) {
+    //     if (err) throw err;
+      
+    //     // object of all the users
+    //     console.log('this is information taken from the db', users);
+    //   });
+    // res.status(201).send('new user added');
     res.send('hello from server 1');
 })
 
 app.post('/', (req, res) => {
-    res.status(201).send(req.body);
+    // create a new user
+    console.log('req.body.server:', req.body.server);
+    console.log('req.body.value', req.body.value);
+    var newUser = User({
+        sender: req.body.server,
+        value: req.body.value
+    });
+   
+    // save the user
+    newUser.save(function(err) {
+        if (err) throw err;
+        console.log('Post created!');
+    });
+    res.status(201).send('post added to db');
 })
--- server/server2.js
+++ server/server2.js
@@ -16,10 +16,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded(extended = true));
 app.use(express.static(path.join(__dirname, 'client')));
 
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/serverChallenge');
+var db = mongoose.connection;
+db.on('error', function (err) {
+    console.log('connection error', err);
+});
+db.once('open', function () {
+    console.log('connected.');
+});
+var Schema = mongoose.Schema;
+// create a schema
+var userSchema = new Schema({
+  sender: String,
+  value: String
+});
+var User = mongoose.model('User', userSchema);
+
+
 app.get('/', (req, res) => {
+    // User.find({}, function(err, users) {
+    //     if (err) throw err;
+      
+    //     // object of all the users
+    //     console.log('this is information taken from the db', users);
+    //   });
+    // res.status(201).send('new user added');
     res.send('hello from server 2');
 })
 
 app.post('/', (req, res) => {
-    res.status(200).send(req.body);
+    // create a new user
+    console.log('req.body.server:', req.body.server);
+    console.log('req.body.value', req.body.value);
+    var newUser = User({
+        sender: req.body.server,
+        value: req.body.value
+    });
+   
+    // save the user
+    newUser.save(function(err) {
+        if (err) throw err;
+        console.log('Post created!');
+    });
+    User.find({}, function(err, users) {
+        if (err) throw err;
+      
+        // object of all the users
+        console.log(users);
+      });
+    res.status(201).send('new post added');
 })
